7b5d8fd7782d6b51530480a3f2db8c3c1e75b38a
Carlos Arantes <Carlos.Arantes@partner.bmwgroup.com>
2025-04-15 20:44:26 +0100

    test

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Instructions
 
+This porject is for studyng purpose ONLY.
+
 - `npm i`
 - `node index.js`
--- index.js
+++ index.js
@@ -13,13 +13,19 @@ const xpaths = {
 };
 var rangeLikes = {
     min: 4,
-    max: 11
+    max: 19
 };
 var rangeDislikes = {
     min: 1,
     max: 3
 };
+var likesCount = 0;
 
+async function sleep(secs){
+    return await new Promise(resolve => setTimeout(() => {
+        resolve()
+    }, secs*1000))
+}
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -30,9 +36,16 @@ var rangeDislikes = {
     await page.goto(tinderPath);
     await page.setViewport();
 
-    await new Promise(resolve => setTimeout(() => {
-        resolve()
-    }, 12000))
+    await sleep(10) // 12 Seconds
+
+    const likeButton = await page.waitForSelector(xpaths.like, {
+        timeout: 10000
+    })
+
+    const dislikeButton = await page.waitForSelector(xpaths.dislike, {
+        timeout: 10000
+    })
+
 
 
     const changeLocation = (city = '') => {
@@ -45,26 +58,15 @@ var rangeDislikes = {
     }
 
     try {
-        const likeButton = await page.waitForSelector(xpaths.like, {
-            timeout: 10000
-        })
-
-        const dislikeButton = await page.waitForSelector(xpaths.dislike, {
-            timeout: 10000
-        })
-    
-        let likesCount = 0;
-        while (likesCount < maxOfLikes || (maxOfLikes < 0)) {            
+        while (likesCount < maxOfLikes || (maxOfLikes < 0 || maxOfLikes == null)) {            
             const rangeOfLikes = random(rangeLikes.min, rangeLikes.max, false)
             console.log("This round we will perform some likes: "+rangeOfLikes)
             for (var i = 0; i < rangeOfLikes; i++) {
                 likeButton.click({
-                    delay: random(290, 410)
+                    delay: random(290, 395)
                 })
                 likesCount++;
-                await new Promise(resolve => setTimeout(() => {
-                    resolve()
-                }, random(1000, 1583, false)))
+                await sleep(random(1000, 1483, false)/1000)
                 console.log(" Likes in this round: "+ (i+1))
             }
 
@@ -72,11 +74,9 @@ var rangeDislikes = {
             console.log("This round we will perform some dislikes: "+rangeOfDislikes)
             for (var x = 0;x < rangeOfDislikes; x++) {
                 dislikeButton.click({
-                    delay: random(310, 450)
+                    delay: random(310, 420)
                 })
-                await new Promise(resolve => setTimeout(() => {
-                    resolve()
-                }, random(1000, 1250, false)))
+                await sleep(random(1000, 1250, false)/1000)
                 console.log(" Dislikes in this round: "+ (x+1))
             }
 
